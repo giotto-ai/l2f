4d6ad679626545fda0e9e22ed6270363ab148314
Helena Peic Tukuljac <hela_su@yahoo.com>
2020-02-20 17:17:13 +0100

    Unittests - causal and anticausal

--- demo_compare_causal_anticausal.py
+++ demo_compare_causal_anticausal.py
@@ -9,15 +9,16 @@ from acoustics import generalized_detrending, generalized_detrending_causal, sor
 df = pd.DataFrame(np.random.rand(10))
 signal = df.to_numpy()[:,0]
 
-ws_detrending = 2
+ws_detrending = 5
 detrending_feature = CrestFactorDetrending(window_size=ws_detrending, is_causal = True) # causal
 detrended_df = detrending_feature.fit_transform(df)
 detrended_signal = generalized_detrending_causal(signal, ws_detrending)                 # MovingCustomFeature #1
 
-ws_sorted_density = 2
+ws_sorted_density = 5
 density_feature = SortedDensity(window_size=ws_sorted_density, is_causal = True)        # causal
 density_df = density_feature.fit_transform(detrended_df)
 echo_density = sorted_density_feature(detrended_signal, ws_sorted_density)              # MovingCustomFeature #2
 
+print('Window sizes: Detrending: ', ws_detrending, ' Density: ', ws_sorted_density)
 print(density_df.to_numpy()[:,0])
 print(echo_density)
